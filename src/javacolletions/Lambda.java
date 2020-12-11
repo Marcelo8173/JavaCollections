77927d312a92a893054acd3ba8b95a67188a110d
Marcelo8173 <marceloandrebio@gmail.com>
2020-12-10 22:03:27 -0300

    regras de função lambdas

--- src/javacolletions/Lambda.java
+++ src/javacolletions/Lambda.java
@@ -11,7 +11,15 @@ public class Lambda {
         //é um metodo abstrado
         Funcao1 fun = valor -> valor;
         System.out.println(fun.gerar("A"));
-        
+        // interfaceFuncional nomeDaVariavel = parametro -> logica
+        //quando é uma logica pequena não precisa de chaves
+        //mais de um parametro e/ou retorno precisa de chaves
+        Funcao1 func1 = valor -> {
+            String valorComPrefixo = "Sr. " + valor;
+            String valorFinal = valorComPrefixo+".";
+            return valorFinal;
+        };
+        System.out.println(func1.gerar("Marcelo"));
     }
 }
 
